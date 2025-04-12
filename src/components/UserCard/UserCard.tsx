40cebc9382f828c2e86eef5c148752ecb9e3d864
unknown <lotfidoucanef@hotmail.fr>
2025-04-13 01:20:16 +0200

    finish to implenent cards and grids, call API is functionnal but debounce is not finished

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,26 +1,27 @@
+import { GithubUser } from '../../types/githubusers.types';
 import './UserCard.css';
 
 type UserCardProps = {
-  avatarUrl?: string;
-  username?: string;
+  userInfo: GithubUser;
 };
-//CHANGER LE TYPE EN FCT DE LA DONNEE ARRIVEE
 
-export default function UserCard({ _test }: UserCardProps) {
+function UserCard({ userInfo }: UserCardProps) {
   return (
     <div className="user-card">
       <input type="checkbox" className="user-card__checkbox" />
 
       <div className="user-card__avatar">
-        <img src="https://www.media.pokekalos.fr/img/pokemon/home/small/salameche.png" className="avatar-img" />
+        <img src={userInfo?.avatar_url} className="avatar-img" />
       </div>
 
       <div className="user-card__info">
-        <div className="user-card__id">ID</div>
-        <div className="user-card__login">Login</div>
+        <div className="user-card__id">{userInfo?.id}</div>
+        <div className="user-card__login">{userInfo?.login}</div>
       </div>
 
       <button className="user-card__button">View profile</button>
     </div>
   );
 }
+
+export default UserCard
--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,8 +1,10 @@
 import config from "../config"
+
 const GithubService = {
-  getProfiles(userName: string) {
+  // I decided to Keep page & perPage at GitHub API defaults (page=1, per_page=30)
+  searchUsers(userName: string, page = 1, perPage = 30) {
     const {baseUrl, endpoints} = config.api
-    return fetch(`${baseUrl}${endpoints.searchUsers}${userName}`);
+    return fetch(`${baseUrl}${endpoints.searchUsers}${userName}&per_page=${perPage}&page=${page}`);
   }
 }
 
